2b87a5afaea3a373d5052cfde0bef66a986a5348
FernandoROL <felhopez@gmail.com>
2025-01-21 15:27:49 -0300

    theme creation

---
+++ footer.php
@@ -0,0 +1,16 @@
+        <footer class="site-footer">
+            <div class="container">
+                <div class="copyright">
+                    <p>Copyright X - All Rights Reserved</p>
+                </div>
+                <nav class="footer-menu">
+                    <?php 
+                        wp_nav_menu(['theme_location' => 'class_footer_menu', 'depth' => 1]);
+                    ?>
+                </nav>
+            </div>
+        </footer>
+    </div>
+    <?php wp_footer() ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php get_header() ?>
+    <div class="site-content" id="content">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main">
+                <section class="hero">
+                    Hero
+                </section>
+                <section class="services">
+                    Service
+                </section>
+                <section class="home-blog">
+                    Blog
+                </section>
+            </main>
+        </div>
+    </div>
+<?php get_footer() ?>
